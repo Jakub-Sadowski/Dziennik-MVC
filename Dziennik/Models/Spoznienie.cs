e921c174fa7c24a79b6a6297825d665163c67b46
kucharz96 <kucharz96@wp.pl>
2018-11-15 13:18:16 +0100

    Create models and some views

---
+++ Dziennik/Models/Spoznienie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dziennik.Models
+{
+    public class Spoznienie
+    {
+        public int ID { get; set; }
+        public int? UczenID { get; set; }
+        public int? LekcjaID { get; set; }
+        public DateTime date { get; set; }
+
+
+        public virtual Uczen Uczen { get; set; }
+        public virtual Lekcja Lekcja { get; set; }
+
+    }
+}
