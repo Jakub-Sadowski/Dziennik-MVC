27ca4113d4c1b68752cabc3841d7a275ea5988a6
Andrzejek Z <zeppenon@o2.pl>
2018-11-29 16:19:16 +0100

    Klasy-> dodawanie,usuwanie(wciaz do fixniecia bug z niektorymi rekordami w db przy usuwaniu),edit (viewsy i kontroler)

--- Dziennik/Models/Klasa.cs
+++ Dziennik/Models/Klasa.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
 namespace Dziennik.Models
 {
     public enum klasa
@@ -17,16 +18,21 @@ namespace Dziennik.Models
         public int? KlasaID { get; set; }
         
         public int? NauczycielID { get; set; }
-        public string nazwa { get; set; }
-      
-        public klasa? level { get; set; }
-
-        public virtual Nauczyciel Nauczyciel{ get; set; }
-        public virtual ICollection<PrzedmiotKlasaNauczyciel> Link { get; set; }
-        public virtual ICollection<Uczen> Uczniowie { get; set; }
         
+        public string nazwa { get; set; }
+        
+        public klasa? level { get; set; }
+        
+        public virtual Nauczyciel Nauczyciel{ get; set; }
+      
+        public virtual ICollection<PrzedmiotKlasaNauczyciel> Link { get; set; }
+     
+        public virtual ICollection<Uczen> Uczniowie { get; set; }
+       
         public virtual ICollection<Lekcja> Lekcje { get; set; }
+      
         public virtual ICollection<Plik> Pliki{ get; set; }
+    
         public virtual ICollection<Test> Testy { get; set; }
 
     }
