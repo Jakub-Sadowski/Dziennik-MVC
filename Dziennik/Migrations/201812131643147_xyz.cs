fad5ea1011e2db5275666fb510d9791e3fbd08f7
kucharz96 <kucharz96@wp.pl>
2018-12-13 18:16:13 +0100

    Baza danych Azure

--- Dziennik/Migrations/201812131643147_xyz.cs
+++ Dziennik/Migrations/201812131643147_xyz.cs
@@ -3,7 +3,7 @@ namespace Dziennik.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class rodzice : DbMigration
+    public partial class xyz : DbMigration
     {
         public override void Up()
         {
