f56ae470181dd87dc911e25afdd4568e0b867275
kucharz96 <kucharz96@wp.pl>
2019-04-07 16:39:29 +0200

    profile uzytkownikow

--- Dziennik/Controllers/HomeController.cs
+++ Dziennik/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Context = Dziennik.DAL.Context;
@@ -14,6 +15,37 @@ namespace Dziennik.Controllers
             var ogloszenia = db.Ogloszenia.Include(o => o.Nauczyciel).OrderByDescending(x => x.data);
             return View(ogloszenia.ToList());
         }
+
+        public ActionResult Profil()
+        {
+
+      
+
+                int id = Int32.Parse((string)Session["UserID"]);
+
+
+                if(Session["Status"] == "Admin")
+                    return RedirectToAction("Profil", "Administrator",new {id = id });
+                else
+                if (Session["Status"] == "Uczen")
+                    return RedirectToAction("Profil", "Uczen", new { id = id });
+                else
+                if (Session["Status"] == "Nauczyciel")
+                    return RedirectToAction("Profil", "Nauczyciel", new { id = id });
+
+                else
+                if (Session["Status"] == "Rodzic")
+                    return RedirectToAction("Profil", "Rodzic", new { id = id });
+                else
+                    return RedirectToAction("Index", "Home");
+
+
+
+
+
+
+        }
+
     }
 }
 
